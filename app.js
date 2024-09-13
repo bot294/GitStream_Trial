cd4726d803bedcd49cb560e9ea3d830ef91c2da8
amoiz068 <amoiz0468@gmail.com>
2024-09-13 11:21:46 +0500

    Added code in app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// Sum two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Example usage
+const number1 = 10;
+const number2 = 20;
+const sum = addNumbers(number1, number2);
+
+console.log(`The sum of ${number1} and ${number2} is ${sum}.`);
+
+// Log a message to the console
+function logMessage(message) {
+  console.log(`Message: ${message}`);
+}
+
+// Example usage
+logMessage("Hello, this is a test message!");
