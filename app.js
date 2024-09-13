ed469f6fa51963ec699158187d06d40cf4f7b02f
amoiz0468 <amoiz0468@gmail.com>
2024-09-13 15:07:30 +0500

    Lines Removed

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ const sum = addNumbers(number1, number2);
 const sub = subNumbers(number1, number2);
 console.log(`The sum of ${number1} and ${number2} is ${sum}.`);
 console.log(`The sub of ${number1} and ${number2} is ${sub}.`);
-
 // Log a message to the console
 function logMessage(message) {
   console.log(`Message: ${message}`);
 }
-
 // Example usage
 logMessage("Hello, this is a test message!");
